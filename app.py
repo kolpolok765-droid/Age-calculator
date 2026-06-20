c6b30ae6e360906ca19dde85e0aa339279b134d4
Cynthia <kolpolok765@gmail.com>
2026-06-20 23:28:20 +0200

    cc

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def calculate_age():
 
 if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=debug)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=debug)
